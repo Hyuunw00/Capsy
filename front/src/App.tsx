1a34193d7491d02eafd1ce8ba7d42ce96e8f812b
ChoiYoungKyu <mpd253@naver.com>
2024-12-07 03:36:34 +0900

    `mpd253/` -> `dev`

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,23 @@
+import { Route, Routes } from "react-router";
+import Main from "./pages/Main";
+import Write from "./pages/Write";
+import Event from "./pages/Event";
+import MyPage from "./pages/MyPage";
+import Login from "./pages/Login";
+import RootLayout from "./layouts/RootLayout";
+
 export default function App() {
   return (
     <>
-      <h1>APP Test.</h1>
-      <h1 className="text-3xl font-bold underline">Admin</h1>;
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
     </>
   );
 }
