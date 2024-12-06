de3b1dd53d63c93dce1ef4c7f7ea6a19d9d12745
ChoiYoungKyu <mpd253@naver.com>
2024-12-07 01:33:54 +0900

    Branch Test

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,7 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      이것도 바뀌었어요.
-    </h1>
-    <h2>
-      예?
-    </h2>
+    <>
+      <h1>APP Test.</h1>
+    </>
   );
 }
