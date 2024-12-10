55ccf3b45907db400246f78e2ece2ee6b4ad8ebf
Soorangmanju <tina971007@naver.com>
2024-12-10 15:32:47 +0900

    🚨 :rotating_light: Fix: [profileHeader] - [아이콘 추가 및 header.tsx 작성]

--- front/src/components/ProfileHeader.tsx
+++ front/src/components/ProfileHeader.tsx
@@ -6,51 +6,70 @@ export default function ProfileHeader() {
   const posts = 19;
   const followers = 99;
   const following = 99;
+  const nickname = "캡시햄찌";
 
   return (
-    <div className="px-[30px] py-6">
-      {/* 프로필 정보 헤더 */}
-      <div className="flex flex-col ">
-        <h2 className="text-xl font-semibold">{username}</h2>
-        {/* 프로필 이미지 */}
-        <div className="relative">
-          <img src={profileImgSample} alt="Profile" className="w-[90px] h-[90px] rounded-full object-cover" />
-          <label
-            htmlFor="profileImageInput"
-            className="absolute bottom-0 right-0 transform translate-x-[30%] translate-y-[30%] bg-white p-1.5 rounded-full cursor-pointer"
-          >
-            <img src={profileImgEditIcon} alt="Edit Icon" className="w-5 h-5" />
-          </label>
-          <input type="file" id="profileImageInput" accept="image/*" className="hidden" />
+    <div className="px-[30px] py-6 mb-[30px]">
+      {/* 헤더 */}
+      <div className="flex flex-col">
+        {/* 유저 이름 */}
+        <h2 className="text-xl font-semibold mb-[20px]">{username}</h2>
+
+        <div className="flex items-center justify-evenly">
+          {/* 프로필 이미지 */}
+          <div className="relative w-[90px] h-[90px]">
+            <img src={profileImgSample} alt="Profile" className="w-full h-full rounded-full object-cover" />
+
+            {/* 편집 아이콘 */}
+            <label
+              htmlFor="profileImageInput"
+              className="absolute bottom-0 right-0 cursor-pointer z-10"
+              style={{
+                width: "30px",
+                height: "30px",
+                marginBottom: "0px",
+                marginRight: "0px",
+              }}
+            >
+              <img src={profileImgEditIcon} alt="Edit Icon" className="w-full h-full" />
+            </label>
+
+            <input type="file" id="profileImageInput" accept="image/*" className="hidden" />
+          </div>
+
+          {/* 중앙: 게시물, 팔로워, 팔로잉 */}
+          <div className="flex flex-1 justify-evenly text-center mt-4">
+            <div className="flex flex-col items-center">
+              <span className="font-semibold text-[14px]">{posts}</span>
+              <span className="font-normal text-[14px]">게시물</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="font-semibold text-[14px]">{followers}</span>
+              <span className="font-normal text-[14px]">팔로워</span>
+            </div>
+            <div className="flex flex-col items-center last:mr-0">
+              <span className="font-semibold text-[14px]">{following}</span>
+              <span className="font-normal text-[14px]">팔로잉</span>
+            </div>
+          </div>
         </div>
 
-        {/* 게시물, 팔로워, 팔로잉 */}
-        <div className="ml-6 flex flex-1 justify-between text-center">
-          <div className="flex flex-col items-center">
-            <span className="font-semibold text-[14px]">{posts}</span>
-            <span className="font-normal text-[14px]">게시물</span>
-          </div>
-          <div className="flex flex-col items-center">
-            <span className="font-semibold text-[14px]">{followers}</span>
-            <span className="font-normal text-[14px]">팔로워</span>
-          </div>
-          <div className="flex flex-col items-center">
-            <span className="font-semibold text-[14px]">{following}</span>
-            <span className="font-normal text-[14px]">팔로잉</span>
-          </div>
+        {/* 닉네임 부분 추가 */}
+        <div className="mt-[20px]">
+          <h3 className="text-[14px] font-regular">{nickname}</h3>
         </div>
-      </div>
 
-      {/* 프로필 편집과 프로필 공유 버튼 */}
-      <div className="flex space-x-[5px] mt-6">
-        {/* 프로필 편집 버튼 */}
-        <button className="flex-1 py-3 text-white text-[14px] font-normal bg-primary rounded-[10px]">
-          프로필 편집
-        </button>
-        {/* 프로필 공유 버튼 */}
-        <button className="flex-1 py-3 text-white text-[14px] font-normal bg-primary rounded-[10px]">
-          프로필 공유
-        </button>
+        {/* 프로필 편집과 프로필 공유 버튼 */}
+        <div className="flex space-x-[5px] mt-6">
+          {/* 프로필 편집 버튼 */}
+          <button className="flex-1 py-3 text-white text-[14px] font-normal bg-primary rounded-[10px]">
+            프로필 편집
+          </button>
+          {/* 프로필 공유 버튼 */}
+          <button className="flex-1 py-3 text-white text-[14px] font-normal bg-primary rounded-[10px]">
+            프로필 공유
+          </button>
+        </div>
       </div>
     </div>
   );
--- front/src/pages/mypage/MyPage.tsx
+++ front/src/pages/mypage/MyPage.tsx
@@ -1,14 +1,15 @@
 import ProfileimageForm from "./modal/ProfileImageForm";
 import ProfileForm from "./modal/ProfileForm";
 import ProfileHeader from "../../components/ProfileHeader";
+import ProfileContainer from "../../components/ProfileContainer";
 
 export default function MyPage() {
   return (
     <>
-      <h1>My Page</h1>
       <ProfileimageForm />
       <ProfileForm />
       <ProfileHeader />
+      <ProfileContainer />
     </>
   );
 }
