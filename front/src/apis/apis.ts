fc1273837014d463216118f6961b4f64f1bf906f
miseullang <miseullang@gmail.com>
2024-12-11 17:43:36 +0900

    :hammer: Refactor: 각 채널의 포스트를 불러오는 함수 추가 / 포스트 타입 정의

--- front/src/apis/apis.ts
+++ front/src/apis/apis.ts
@@ -1,7 +1,7 @@
 import axiosInstance from "./axiosInstance";
 
-export const CHANNEL_ID_TIMECAPSULE = '67585b36757bff0e678a56a8';
-export const CHANNEL_ID_POST = '67585a88757bff0e678a56a3';
+export const CHANNEL_ID_TIMECAPSULE = "67585b36757bff0e678a56a8";
+export const CHANNEL_ID_POST = "67585a88757bff0e678a56a3";
 
 // 포스트 생성 API
 interface CreatePostRequest {
@@ -21,7 +21,7 @@ export const createPost = async (data: FormData | CreatePostRequest) => {
       });
       return response.data;
     }
-    
+
     // 이미지가 없는 경우 => JSON로 전송
     const response = await axiosInstance.post("/posts/create", data);
     return response.data;
@@ -30,23 +30,34 @@ export const createPost = async (data: FormData | CreatePostRequest) => {
   }
 };
 
-
 // 특정 사용자의 포스트 목록 조회 API
-export const getUserPosts = async (authorId:string) => {
+export const getUserPosts = async (authorId: string) => {
   try {
     const response = await axiosInstance.get(`/posts/author/${authorId}`);
     return response.data;
   } catch (error) {
     throw error;
   }
-}
+};
 
-// 특정 포스트 조회 API
-export const getPostDetail = async (postId : string) => {
+// 일반 채널의 포스트 목록 조회 API
+export const getTimeCapsuleChannel = async () => {
   try {
-    const response = await axiosInstance.get(`/post/${postId}`);
+    const response = await axiosInstance.get(`/posts/channel/${CHANNEL_ID_TIMECAPSULE};
+    }`);
     return response.data;
   } catch (error) {
     throw error;
   }
-}
+};
+
+// 일반 채널의 포스트 목록 조회 API
+export const getPostChannel = async () => {
+  try {
+    const response = await axiosInstance.get(`/posts/channel/${CHANNEL_ID_POST};
+    }`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
