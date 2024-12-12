dfebdd871e54e7d7d37856b675effe64fb73ac61
ChoiYoungKyu <mpd253@naver.com>
2024-12-12 21:21:26 +0900

    ✨ Feature: 메인에서 상세페이지 navigate, 게시글 이미지 없을 때 이미지, 게시글 글자 수 제한, 게시글 round 처리

--- front/src/pages/main/MainPage.tsx
+++ front/src/pages/main/MainPage.tsx
@@ -13,6 +13,7 @@ import img_fillNoti from "../../assets/Notification-fill.svg";
 import img_scroll from "../../assets/scroll-icon.svg";
 import { useMainSearchStore } from "../../store/mainSearchStore";
 import MainSearchModal from "./MainSearchModal";
+import { useNavigate } from "react-router-dom";
 
 interface Like {
   _id: string;
@@ -68,6 +69,7 @@ interface Post {
 }
 
 export default function MainPage() {
+  const navigate = useNavigate();
   // 전체 게시글 데이터 (현재는 dummyData)
   const [data, setData] = useState<Post[]>([]);
   // 일반 게시글 post 데이터
@@ -80,10 +82,10 @@ export default function MainPage() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   // 필터링 드롭다운 선택한 옵션
   const [selectedOption, setSelectedOption] = useState<string>("All");
-  // 각 게시물 알림 상태 관리
+  // // 각 게시물 알림 상태 관리
   const [notiStatus, setNotiStatus] = useState<boolean[]>([]);
 
-  // 각 게시물 좋아요 상태 관리
+  // 각 게시물 좋아요, 알림 상태 관리
   const [userData, _] = useState(() => {
     const storedUserData = sessionStorage.getItem("user");
     return storedUserData ? JSON.parse(storedUserData) : { likes: [] };
@@ -123,9 +125,7 @@ export default function MainPage() {
 
   // 좋아요 버튼 클릭 이벤트 핸들러
   const handleLikeClick = async (postId: string) => {
-    console.log("postId: ", postId);
     const userId = userData._id;
-    console.log("My ID: ", userId);
 
     // 전체 데이터와 클릭한 post id 비교
     const post = filterData.find((post) => post._id === postId);
@@ -135,7 +135,6 @@ export default function MainPage() {
 
     // 유저가 해당 게시글 좋아요 눌렀었는지 확인
     const userLikes = post?.likes.filter((like) => like.user === userId);
-    console.log("userLikes", userLikes);
 
     try {
       // 좋아요를 누르지 않았다면 추가
@@ -173,6 +172,30 @@ export default function MainPage() {
     });
   };
 
+  // 게시글 제목 가져오기
+  const getTitle = (jsonString: any) => {
+    try {
+      const parsedData = JSON.parse(jsonString);
+      return parsedData.title || jsonString;
+    } catch (error) {
+      // 기존의 데이터가 잘못 들어가있어 console을 잡아먹어 주석 처리
+      // console.error("JSON parse error: ", error);
+      return jsonString;
+    }
+  };
+
+  // 게시글 내용 가져오기
+  const getContent = (jsonString: any) => {
+    try {
+      const parsedData = JSON.parse(jsonString);
+      return parsedData.content || jsonString;
+    } catch (error) {
+      // 기존의 데이터가 잘못 들어가있어 console을 잡아먹어 주석 처리
+      // console.error("JSON parse error: ", error);
+      return jsonString;
+    }
+  };
+
   // 데이터 call
   useEffect(() => {
     const updateData = async (postChannelId: string, capsuleChannelId: string) => {
@@ -208,7 +231,7 @@ export default function MainPage() {
       return acc;
     }, {});
     setLikeStatus(newLikeStatus);
-  }, [filterData, userData]);
+  }, [filterData]);
 
   // 필터링 로직
   useEffect(() => {
@@ -223,8 +246,9 @@ export default function MainPage() {
 
   // 데이터 확인용 console.log
   useEffect(() => {
-    console.log("filterData", filterData);
+    // console.log("filterData", filterData);
     console.log("userData", userData);
+    console.log("capsuleData", capsuleData);
   }, [data, filterData]);
 
   // 검색된 게시물에대한 코드
@@ -299,8 +323,22 @@ export default function MainPage() {
         {/* 게시물 data */}
         <div className="columns-2 gap-x-[20px] mt-3">
           {filterData.map((item, index) => (
-            <div key={index} className="w-full inline-block break-inside-avoid  relative mb-[10px]">
-              <img src={item.image} alt={item.title} className="w-full h-auto object-cover" />
+            <div
+              key={index}
+              className="w-full inline-block break-inside-avoid relative mb-[10px] cursor-pointer"
+              onClick={() => navigate(`/detail/${item._id}`)}
+            >
+              {item.image ? (
+                <img src={item.image} alt={item.title} className="w-full h-auto rounded-[10px] object-cover" />
+              ) : (
+                <div className="w-full h-40 bg-gray-200 rounded-[10px] relative">
+                  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-xl ">
+                    <p className="overflow-hidden text-ellipsis whitespace-nowrap" style={{ maxWidth: "calc(12ch)" }}>
+                      {item.title ? getContent(item.title) : "텍스트 없음"}
+                    </p>
+                  </div>
+                </div>
+              )}
               {item.channel.name === "TIMECAPSULE" && (
                 <div className="absolute top-[6px] right-[6px]">
                   <img src={img_capsule} alt="캡슐" />
@@ -308,7 +346,9 @@ export default function MainPage() {
               )}
               <div className="absolute bottom-[6px] left-[6px] w-full text-white px-1">
                 <p className="font-semibold">@{item.author.fullName}</p>
-                <p>{item.title}</p>
+                <p className="overflow-hidden text-ellipsis whitespace-nowrap" style={{ maxWidth: "calc(18ch)" }}>
+                  {getTitle(item.title)}
+                </p>
               </div>
               <div className="absolute bottom-[6px] right-[6px] flex flex-col justify-center items-center space-y-1">
                 <img
@@ -316,11 +356,11 @@ export default function MainPage() {
                   className="w-[20px] h-[20px] object-contain cursor-pointer"
                   onClick={() => handleLikeClick(item._id)}
                 />
-                {item.channel.name === "capsule" && (
+                {item.channel.name === "TIMECAPSULE" && (
                   <img
                     src={notiStatus[index] ? img_fillNoti : img_noti}
                     alt="noti"
-                    className={`object-contain cursor-pointer h-[20px] ${notiStatus[index] ? "w-[16px]" : "w-[18px]"}`}
+                    className={`object-contain cursor-pointer h-[20px] ${notiStatus[index] ? "w-[15px]" : "w-[18px]"}`}
                     onClick={() => handleNotiClick(index)}
                   />
                 )}
