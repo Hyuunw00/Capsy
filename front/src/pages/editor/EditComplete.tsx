5603936be3cff19b1fc58e6ed5deb63dcf35c808
miseullang <miseullang@gmail.com>
2024-12-13 16:50:16 +0900

    :rotating_light: Fix: 작성 완료시 알림 팝업 띄우기

--- front/src/pages/editor/EditComplete.tsx
+++ front/src/pages/editor/EditComplete.tsx
@@ -5,13 +5,13 @@ interface EditCompleteProps {
   isOpen: boolean;
   onClose: () => void;
   isTimeCapsule: boolean;
+  postId : string;
 }
 
-const EditComplete = ({ isOpen, onClose, isTimeCapsule }: EditCompleteProps) => {
+const EditComplete = ({ isOpen, onClose, isTimeCapsule, postId }: EditCompleteProps) => {
   const navigate = useNavigate();
 
   const handleConfirm = () => {
-    const postId = "123"; // 임시 ID
     navigate(`/detail/${postId}`);
   };
 
@@ -22,11 +22,11 @@ const EditComplete = ({ isOpen, onClose, isTimeCapsule }: EditCompleteProps) => 
         title="타임캡슐은 수정할 수 없습니다"
         description={`타임캡슐은 지정한 날짜가 도래하기 전까지\n내용을 수정할 수 없습니다.\n정말 작성을 완료하시겠습니까?`}
       >
-        <div className="flex flex-col gap-2">
-          <button onClick={onClose} className="w-full py-2 border rounded-md border-primary text-primary">
+        <div className="gap-2 item-between">
+          <button onClick={onClose} className="w-full py-2 border border-black rounded-sm">
             취소
           </button>
-          <button onClick={handleConfirm} className="w-full py-2 text-white rounded-md bg-primary">
+          <button onClick={handleConfirm} className="w-full py-2 text-white bg-black rounded-sm">
             완료
           </button>
         </div>
@@ -40,7 +40,7 @@ const EditComplete = ({ isOpen, onClose, isTimeCapsule }: EditCompleteProps) => 
       title="저장이 완료되었습니다"
       description="확인 버튼을 누르면 해당 포스트로 이동합니다"
     >
-      <button onClick={handleConfirm} className="w-full py-2 text-white rounded-md bg-primary">
+      <button onClick={handleConfirm} className="w-full py-2 text-white bg-black rounded-md">
         확인
       </button>
     </NotificationModal>
