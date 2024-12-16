56401c2e4f1b484b375756bf5984de3ca4fd6fd5
miseullang <miseullang@gmail.com>
2024-12-16 16:56:20 +0900

    :rotating_light:Fix: 다중 이미지 처리 및 좋아요 충돌 해결

--- front/src/components/Loading.tsx
+++ front/src/components/Loading.tsx
@@ -1,9 +1,12 @@
 import loadingImg from '../assets/loading-icon-black.svg';
+import loadingImgWhite from '../assets/loading-icon.svg';
+import { useThemeStore } from '../store/themeStore'
 
 const Loading = () => {
+  const { isDark } = useThemeStore();
   return (
-    <div className='absolute top-0 left-0 z-30 w-full bg-white abs h-dvh item-middle'>
-      <img src={loadingImg} alt="로딩 이미지" />
+    <div className='absolute top-0 left-0 z-30 w-full bg-white abs h-dvh item-middle dark:bg-black'>
+      <img src={isDark ? loadingImgWhite : loadingImg} alt="로딩 이미지" />
     </div>
   );
 }
