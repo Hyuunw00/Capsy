0689890058fb47c6cbfd894f461ded7f21be74db
Soorangmanju <tina971007@naver.com>
2024-12-16 22:48:32 +0900

    :rotating_light: Fix: [mypage] - [캡슐글 공개완료/대기 분리,일반글 탭]

--- front/src/pages/mypage/CapsuleListPage.tsx
+++ front/src/pages/mypage/CapsuleListPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { getTimeCapsuleChannel, CHANNEL_ID_TIMECAPSULE } from "../../apis/apis";
 
 interface CapsuleListPageState {
   title: string; // 제목 (ex. "공개 완료", "공개 대기")
