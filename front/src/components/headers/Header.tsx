025d0cf7ecfda30ab5f09156cfa3e197dc1c8997
miseullang <miseullang@gmail.com>
2024-12-17 12:14:38 +0900

    :lipstick:Style: 헤더 CSS 수정

--- front/src/components/headers/Header.tsx
+++ front/src/components/headers/Header.tsx
@@ -157,7 +157,7 @@ export default function Header() {
       <nav className="absolute top-0 z-20 justify-between w-full px-8 py-4 bg-white dark:bg-black item-between">
         <img src={logo_black} alt="Logo" className="w-[75px] h-[30px] dark:invert" />
 
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
           <button onClick={toggleTheme} className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-600">
             <img
               src={isDark ? LightMode : DarkMode}
@@ -168,7 +168,7 @@ export default function Header() {
           <button
             onClick={() => isLoggedIn && setShowNoticeModal((prev) => !prev)}
             className={`flex items-center justify-center w-5 h-5 relative ${
-              !isLoggedIn ? "invisible pointer-events-none" : ""
+              !isLoggedIn ? "hidden" : ""
             }`}
           >
             <img src={NotificationIcon} alt="Notification" className="object-contain w-full h-full dark:invert" />
