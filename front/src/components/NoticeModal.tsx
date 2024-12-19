40b3c36438e45a2156475502ea2af86a123ceffc
허정민 <gjwjdals96@naver.com>
2024-12-19 23:40:33 +0900

    :lipstick: Style:로그인 및 회원가입 탭 다크모드 보완

--- front/src/components/NoticeModal.tsx
+++ front/src/components/NoticeModal.tsx
@@ -10,10 +10,13 @@ const NoticeModal = ({ title, children, onClose }: NoticeModalProps) => {
   return (
     <>
       <div className="fixed inset-0 z-40 bg-black/30" />
-      <div className="absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 shadow-md z-50 w-[280px] p-4 bg-white rounded-lg">
-        <h5 className="mb-3 text-black">{title}</h5>
-        <p className="mb-4">{children}</p>
-        <button onClick={onClose} className="w-full py-2 text-white bg-black rounded">
+      <div className="absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 shadow-md z-50 w-[350px] p-4 bg-white dark:bg-gray-700 rounded-lg">
+        <h5 className="mb-3 text-black dark:text-white text-center">{title}</h5>
+        <p className="mb-4 dark:text-gray-200 text-center">{children}</p>
+        <button
+          onClick={onClose}
+          className="w-full py-2 text-white dark:text-black bg-primary dark:bg-secondary rounded"
+        >
           확인
         </button>
       </div>
