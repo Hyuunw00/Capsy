6eea723799b075f390b9989b8337e1db2c3ef612
miseullang <miseullang@gmail.com>
2024-12-20 20:38:34 +0900

    :rotating_light:Fix: 로딩 컴포넌트 길이 조정 / 팔로우 알림 멘트 수정

--- front/src/components/headers/NotifyModal.tsx
+++ front/src/components/headers/NotifyModal.tsx
@@ -63,19 +63,19 @@ const NotifyModal = ({
       case "FOLLOW":
         return (
           <div key={notificationKey} className="flex items-center justify-between py-2">
-            <p>{followerNames[notification.userId] || notification.userId}님이 팔로우를 요청했습니다</p>
+            <p>{followerNames[notification.userId] || notification.userId}님이 회원님을 팔로우했습니다.</p>
             <div className="flex gap-2">
               <Button
                 onClick={() => onRejectFollow(notification)}
                 className="px-2 py-0.5 h-7 items-center text-black rounded w-fit border border-black box-border text-sm"
               >
-                거절
+                확인
               </Button>
               <Button
                 onClick={() => handleAcceptFollow(notification)}
                 className="px-2 py-0.5 h-7 items-center text-white rounded w-fit bg-black text-sm"
               >
-                확인
+                이동
               </Button>
             </div>
           </div>
